fd6586222dc376191a881ee87f9dea0f3c7a2062
Waqas <wakassadik@gmail.com>
2021-05-15 19:17:31 +0500

    code changes to handle dropdown close by click on anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -6,7 +6,21 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
+  /** simple closing and opening of dropdown x
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
+  **/
+
+  /** 
+  Closing the Dropdown From Anywhere
+  as we are using ElementRef, we have to add it in constructor to later use it
+  */
+     
+      @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+      }
+    
+      constructor(private elRef: ElementRef) {}
+    
 }
