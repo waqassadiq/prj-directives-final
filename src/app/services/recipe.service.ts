b1934bb1c9b7bc3b75e32d226519f6b1aeb954a6
Waqas <wakassadik@gmail.com>
2021-05-19 10:31:49 +0500

    ingredients are being show for each recipe

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -3,6 +3,7 @@ import {
   EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipes/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,17 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Shawarma', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    [
+      new Ingredient('Doe', 3),
+      new Ingredient('Meat', 1),
+    ]),
+    new Recipe('Burger', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Potato', 3),
+    ])
+    
   ];
 
   getRecipies(){
